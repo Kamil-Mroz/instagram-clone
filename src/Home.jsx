2dc06c793506ca29ee365903f5f6b6c3114c7aad
Kamil Mróz <kamilmp17@gmail.com>
2022-10-09 14:55:49 +0200

    useFetch hook and fetching data

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,28 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useFetch } from './hooks/useFetch'
+import {
+  FaRegHeart,
+  FaRegComment,
+  FaRegBookmark,
+  FaRegPaperPlane,
+} from 'react-icons/fa'
 export const Home = () => {
-  return <div>Home</div>
+  const posts = useFetch('https://jsonplaceholder.typicode.com/posts')
+  const albums = useFetch('https://jsonplaceholder.typicode.com/albums')
+  const users = useFetch('https://jsonplaceholder.typicode.com/users')
+  const photos = useFetch('https://jsonplaceholder.typicode.com/photos')
+  const comments = useFetch('https://jsonplaceholder.typicode.com/comments')
+
+  users?.push({
+    id: 11,
+    name: 'Kamil Mróz',
+    username: 'KamilPM',
+    email: 'Kamil@example.com',
+    phone: '1-770-736-8031 x56442',
+    website: 'https://github.com/Kamil-Mroz',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, ratione quibusdam alias quas similique provident aperiam! Adipisci nobis deserunt porro facilis sit alias consequatur dolorum aliquam laudantium iusto? Iusto, quia?',
+  })
+
+  return <div className="small-container"></div>
 }
---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+export const useFetch = (url) => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err))
+  }, [url])
+  return data
+}
