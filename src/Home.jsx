17c08e437963587782831634ee3ea16fe59120fb
Kamil Mróz <kamilmp17@gmail.com>
2022-10-14 19:20:50 +0200

    changed the obverver

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect, useRef, useMemo } from 'react'
 import { DataContext } from './Data'
 import { Card } from './Card'
 export const Home = () => {
-  const POSTS_ON_RENDER = 5
+  const POSTS_ON_RENDER = 3
   const [amount, setAmount] = useState(POSTS_ON_RENDER)
   const { posts } = useContext(DataContext)
   const postsLength = posts?.length
@@ -10,27 +10,18 @@ export const Home = () => {
 
   const postsContainerRef = useRef()
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const entry = entries[0]
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0]
 
-        if (posts?.length > 0 && entry.isIntersecting) {
-          console.log('ok')
-          setAmount((prev) => {
-            if (prev + POSTS_ON_RENDER >= postsLength) {
-              return postsLength
-            } else {
-              return prev + POSTS_ON_RENDER
-            }
-          })
-        }
-      },
-      {
-        rootMargin: '300px',
+      if (entry.isIntersecting) {
+        console.log('ok')
+        setAmount((prev) => {
+          return prev + POSTS_ON_RENDER
+        })
       }
-    )
+    }, {})
     observer.observe(postsContainerRef.current)
-  }, [posts])
+  }, [])
   return (
     <div className="small-container">
       {newPosts?.map((post) => (
